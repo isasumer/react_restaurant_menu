e3c5962b4891a93bcd275607805645cc9917cf55
Isa SUMER <sumerisa5@gmail.com>
2021-09-21 10:48:11 +0300

    router addded

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import {useState} from 'react';
-import Navbar from "./components/Navbar"
-import Header from "./components/Header"
-import Category from "./components/Category"
-import Menu from "./components/Menu"
-import Cart from "./pages/Cart"
-import data from "./components/data"
-import styled from "styled-components";
-
-const Container = styled.div`
-/* height:100vh; */
-background-color:orange;
-display:flex;
-flex-direction:column;
-align-items: center;
-`;
-const categoryList =["All", ...new Set(data.map((item)=>item.category))]
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
-  const [menuItems, setmenuItems] = useState(data)
-
-  const changeList = (category) => {
-    category === "All"
-      ? setmenuItems(data)
-      : setmenuItems(data.filter((item) => item.category === category));
-      console.log(menuItems)
-  };
-
   return (
-    <Container>
-      <Navbar/>
-      <Header/>
-      <Category categoryList={categoryList} changeList={changeList}/>
-      <Menu menuItems={menuItems}/>
-      <Cart />
-    </Container>
-  )
-}
-
-export default App
+    <Router>
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/" component={Home} />
+    </Router>
+  );
+};
+export default App;
