1388a53ab027b26808526390c1bf911e30a46cf3
Isa SUMER <sumerisa5@gmail.com>
2021-10-06 12:58:32 +0300

    button shaked

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -47,24 +47,30 @@ const Button = styled.button`
   height: 26px;
   cursor: pointer;
   :hover {
-  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
-  transform: translate3d(0, 0, 0);
-  perspective: 1000px;
-}
-@keyframes shake {
-  10%, 90% {
-    transform: translate3d(-1px, 0, 0);
+    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+    transform: translate3d(0, 0, 0);
+    perspective: 1000px;
   }
-  20%, 80% {
-    transform: translate3d(2px, 0, 0);
+  @keyframes shake {
+    10%,
+    90% {
+      transform: translate3d(-1px, 0, 0);
+    }
+    20%,
+    80% {
+      transform: translate3d(2px, 0, 0);
+    }
+
+    30%,
+    50%,
+    70% {
+      transform: translate3d(-2px, 0, 0);
+    }
+    40%,
+    60% {
+      transform: translate3d(2px, 0, 0);
+    }
   }
-  
-  30%, 50%, 70% {
-    transform: translate3d(-2px, 0, 0);
-  }
-  40%, 60% {
-    transform: translate3d(2px, 0, 0);
-}
 `;
 
 const Menu = (props) => {
