cba68980417c6e7f41fb2ac2b8b02efe337bcdff
Isa SUMER <sumerisa5@gmail.com>
2021-10-08 00:56:15 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <Route exact path="/cart" component={Cart} />
+    <Router basename={process.env.PUBLIC_URL}>
       <Route exact path="/" component={Home} />
+      <Route exact path="/cart" component={Cart} />
     </Router>
   );
 };
